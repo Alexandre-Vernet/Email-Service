601e00183039b0d7d2d45f935cbf4b957b8a3e44
Alexandre Vernet <alexandre.vernet99@gmail.com>
2023-04-12 21:37:31 +0200

    feat(mail): Send email with nodemailer

--- server.js
+++ server.js
@@ -1,10 +1,58 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const nodemailer = require("nodemailer");
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json());
+
+app.post('/', (req, res) => {
+        const { name, email, message } = req.body;
+        const { EMAIL, USER, PASSWORD, NODE_ENV } = process.env;
+
+        const mailOptions = {
+            from: email,
+            to: EMAIL,
+            subject: 'New message from your portfolio',
+            text: message
+        };
+
+        let transporter;
+        if (NODE_ENV === 'production') {
+            transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: USER,
+                    pass: PASSWORD
+                }
+            });
+        } else {
+            console.log({ EMAIL: USER, PASSWORD, NODE_ENV })
+            transporter = nodemailer.createTransport({
+                host: "sandbox.smtp.mailtrap.io",
+                port: 2525,
+                auth: {
+                    user: USER,
+                    pass: PASSWORD
+                }
+            });
+        }
+
+        transporter.sendMail(mailOptions, (error) => {
+            if (error) {
+                console.error(error);
+                res.send({
+                    message: `Email not sent: ${ error }`
+                });
+            } else {
+                res.send({
+                    message: 'Email sent: '
+                });
+            }
+        });
+    }
+);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${ port }`);
